6b67e94b960b513cebf02643cdcd947bb9045861
Stanislav Popov <popstas@gmail.com>
2018-03-10 07:40:59 +0500

    feat: подстановка проекта из toggl, если по задаче уже была работа

--- src/scripts/content/planfix.js
+++ src/scripts/content/planfix.js
@@ -7,10 +7,13 @@ togglbutton.render('.taskCard-header-data-blocks .b-green-block-taskAfterLabel',
     numElem = $('.b-task-general-id a'),
     projectElem = $('.task-project-text a'),
     existingTag = $('.b-toggl-btn.toggl.planfix'),
-    titleElem;
+    taskId,
+    titleElem,
+    projectName = "",
+    buttonOpts = {};
 
   titleElem = $('.b-task-param-editable a');
-  if(titleElem == null){
+  if (titleElem == null) {
     // for users
     titleElem = $('.taskCard-header-data-blocks > .b-green-block > div > div')
   }
@@ -25,21 +28,39 @@ togglbutton.render('.taskCard-header-data-blocks .b-green-block-taskAfterLabel',
   description = titleElem.textContent;
   if (numElem !== null) {
     description = description.trim();
-    tags = [ numElem.textContent.replace('#', '') ];
+    taskId = numElem.textContent.replace('#', '');
+    tags = [taskId];
   }
 
   div = document.createElement("div");
   div.classList.add("b-toggl-btn", "toggl", "planfix");
   div.style.display = "flex";
+  elem.prepend(div);
 
-  link = togglbutton.createTimerLink({
+  buttonOpts = {
     className: 'planfix',
     description: description,
     buttonType: 'minimal',
     projectName: projectElem && projectElem.textContent,
     tags: tags
-  });
+  };
 
-  div.appendChild(link);
-  elem.prepend(div);
+  fetch('https://localhost:8097/api/v1/toggl/entries/planfix/' + taskId + '/last')
+    .then(response => {
+      if (response.status != 200) {
+        throw new Error('Failed to fetch ' + response.url + ', status ' + response.status);
+      }
+      return response.json();
+    })
+    .then(json => {
+      if (json && json.project) {
+        buttonOpts.projectName = json.project;
+        buttonOpts.description = json.description;
+      }
+      link = togglbutton.createTimerLink(buttonOpts);
+      div.appendChild(link);
+    }, reject => {
+      link = togglbutton.createTimerLink(buttonOpts);
+      div.appendChild(link);
+    });
 });
